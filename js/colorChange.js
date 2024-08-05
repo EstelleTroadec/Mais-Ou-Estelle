a858b92a923ac0ce538a99589258ba2d478dec2f
Estelle Troadec <estelle.t29@gmail.com>
2024-08-05 11:24:58 +0200

    feat: also change the footer color when a color button is clicked + update css style

--- js/colorChange.js
+++ js/colorChange.js
@@ -6,21 +6,26 @@ const colorChange = {
         const greenBtn = document.getElementById('color-change__green');
         const header = document.querySelector('header');
         const cloud = document.querySelector('.header__cloud');
+        const footer = document.querySelector('footer');
         
         // handle color change of the header
         redBtn.addEventListener('click', function () {
             header.style.backgroundColor = '#b58284';
             cloud.style.fill = '#f0cdcf';
+            footer.style.backgroundColor = '#b58284';
+
         });
         
         blueBtn.addEventListener('click', function () {
             header.style.backgroundColor = '#8ee4f5';
             cloud.style.fill = '#c4eff9';
+            footer.style.backgroundColor = '#8ee4f5';
         });
         
         greenBtn.addEventListener('click', function () {
             header.style.backgroundColor = '#6C977D';
             cloud.style.fill = '#a5b7ac';
+            footer.style.backgroundColor = '#6C977D';
         });
     }
 
