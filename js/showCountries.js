d20beda57b846e65d90278c89e4eec1647c75b5d
Estelle Troadec <estelle.t29@gmail.com>
2024-08-05 11:18:00 +0200

    feat: all the country lists will now close when we click on a non-related continent in the menu

--- js/showCountries.js
+++ js/showCountries.js
@@ -1,11 +1,22 @@
 const showCountries = {
     init: function() {
         const continents = document.querySelectorAll('.continent');
+        const allCountryLists = document.querySelectorAll('.continent + ul');
 
         // when we click on a continent name, the list of countries appears or disappears 
         continents.forEach(continent => {
-            continent.addEventListener('click', function () {
-                const ul = this.nextElementSibling;
+            continent.addEventListener('click', function (event) {
+                const ul = event.target.nextElementSibling;
+
+                // close all other country lists
+                allCountryLists.forEach(countryList => {
+                    if (countryList !== ul) {
+                        countryList.style.display = 'none';
+                        countryList.setAttribute('aria-hidden', true);
+                    }
+                });
+
+                // toggle the country list
                 if (ul && ul.tagName === 'UL') {
                     const isHidden = ul.style.display === 'none' || ul.style.display === '';
                     ul.style.display = isHidden ? 'block' : 'none';
