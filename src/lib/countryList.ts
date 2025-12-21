e6bf011a56db35e309a1376299ea809984f5a37b
Estelle Troadec <estelle.t29@gmail.com>
2025-12-21 16:25:09 +0100

    update: add spain page + seville post

--- src/lib/countryList.ts
+++ src/lib/countryList.ts
@@ -6,21 +6,28 @@ const continentCountryMap = {
   ].sort((a, b) => a.name.localeCompare(b.name)),
   "Amérique Centrale & Caraïbes": [
     { name: "Costa Rica", link: "/countries/costa-rica" }
+  /* { name: "Cuba", link: "/countries/cuba" },
+    { name: "Guatemala, link: "/countries/guatemala" },
+    { name: "République Dominicaine", link: "/countries/republique-dominicaine" },
+    { name: "Brésil", link: "/countries/bresil" },  */
   ].sort((a, b) => a.name.localeCompare(b.name)),
   "Amérique du Sud": [
     /* { name: "Brésil", link: "/countries/bresil" }, */
     { name: "Colombie", link: "/countries/colombie" }
   ].sort((a, b) => a.name.localeCompare(b.name)),
-  /* "Europe": [
-    { name: "France", link: "/countries/france" },
-    { name: "Espagne", link: "/countries/espagne" }
-  ].sort((a, b) => a.name.localeCompare(b.name)), */
-  /* "Asie": [
+   "Europe": [
+    { name: "Espagne", link: "/countries/espagne" },
+  /* { name: "Portugal", link: "/countries/portugal" },
+    { name: "Scandinavie", link: "/countries/scandinavie" },
+    { name: "Benelux", link: "/countries/benelux" },
+    { name: "Royaume-Uni", link: "/countries/royaume-uni" },  */
+  ].sort((a, b) => a.name.localeCompare(b.name)),
+  "Asie": [
     { name: "Thailande", link: "/countries/thailande" },
-    { name: "Singapour", link: "/countries/singapour" },
+  /*   { name: "Singapour", link: "/countries/singapour" },
     { name: "Malaisie", link: "/countries/malaisie" },
-    { name: "Bali", link: "/countries/bali" }
-  ].sort((a, b) => a.name.localeCompare(b.name)), */
+    { name: "Bali", link: "/countries/bali" } */
+  ].sort((a, b) => a.name.localeCompare(b.name)),
   "Océanie": [
     { name: "Polynésie Française", link: "/countries/polynesie-francaise" }
   ].sort((a, b) => a.name.localeCompare(b.name)),
